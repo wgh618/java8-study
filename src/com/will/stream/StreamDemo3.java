45cf76c585482a953080087f0a42221c8303e845
willWu <willwu618@gmail.com>
2017-12-13 21:35:29 +0800

    [MOD]Stream API（3）

--- src/com/will/stream/StreamDemo3.java
+++ src/com/will/stream/StreamDemo3.java
@@ -2,6 +2,9 @@ package com.will.stream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * ClassName:StreamDemo2
@@ -31,7 +34,7 @@ public class StreamDemo3 {
      * forEach是一个最终操作，所以我们不能在forEach之后来执行其他Stream操作。
      */
     public static void filter() {
-        stringList.stream().filter(e -> e.startWith("a")).forEach(System.out::println);
+        stringList.stream().filter(e -> e.startsWith("a")).forEach(System.out::println);
         System.out.println(stringList);
     }
 
@@ -40,7 +43,7 @@ public class StreamDemo3 {
      * 需要注意的是，排序只创建了一个排列好后的Stream，而不会影响原有的数据源，排序之后原数据stringCollection是不会被修改的
      */
     public static void sort() {
-        stringList.stream().sorted().filter(e -> e.startWith("a")).forEach(System.out::println);
+        stringList.stream().sorted().filter(e -> e.startsWith("a")).forEach(System.out::println);
         System.out.println(stringList);
     }
 
@@ -49,7 +52,11 @@ public class StreamDemo3 {
      * 将字符串转换为大写字符串，并逆序排序
      */
     public static void map() {
-        stringList.stream().sorted().filter(e -> e.startWith("a")).forEach(System.out::println);
+        stringList
+                .stream()
+                .map(String::toUpperCase)
+                .sorted((a, b) -> b.compareTo(a))
+                .forEach(System.out::println);
         System.out.println(stringList);
     }
 
@@ -65,14 +72,14 @@ public class StreamDemo3 {
         System.out.println(anyStartsWithA);
 
         boolean allStartsWithA = stringList
-                        .stream()
-                        .allMatch((s) -> s.startsWith("a"));
+                .stream()
+                .allMatch((s) -> s.startsWith("a"));
         // false
         System.out.println(allStartsWithA);
 
         boolean noneStartsWithZ = stringList
-                        .stream()
-                        .noneMatch((s) -> s.startsWith("z"));
+                .stream()
+                .noneMatch((s) -> s.startsWith("z"));
         // true
         System.out.println(noneStartsWithZ);
 
@@ -84,15 +91,69 @@ public class StreamDemo3 {
      */
     public static void count() {
         long startsWithB = stringList
-                        .stream()
-                        .filter((s) -> s.startsWith("b"))
-                        .count();
+                .stream()
+                .filter((s) -> s.startsWith("b"))
+                .count();
         // 3
         System.out.println(startsWithB);
 
         System.out.println(stringList);
     }
 
+    /**
+     * Reduce规约:这是一个最终操作，允许通过指定的函数来将stream中的多个元素规约为一个元素，规越后的结果是通过Optional接口表示的：
+     */
+    public static void reduce() {
+        Optional<String> reduce = stringList
+                .stream()
+                .sorted()
+                .reduce((a, b) -> a + "&" + b);
+        //aaa1&aaa2&bbb1&bbb2&bbb3&ccc1&ddd1&ddd2
+        reduce.ifPresent(System.out::println);
+        System.out.println(stringList);
+    }
+
+    /**
+     * 并行Streams:前面提到过Stream有串行和并行两种，串行Stream上的操作是在一个线程中依次完成，而并行Stream则是在多个线程上同时执行
+     */
+    public static void parallel() {
+        int max = 1000000;
+        List<String> values = new ArrayList<>(max);
+        for (int i = 0; i < max; i++) {
+            UUID uuid = UUID.randomUUID();
+            values.add(uuid.toString());
+        }
+
+        // 串行操作
+        long t0 = System.nanoTime();
+
+        long count = values.stream().sorted().count();
+        System.out.println(count);
+
+        long t1 = System.nanoTime();
+
+        long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
+        System.out.println(String.format("sequential sort took: %d ms", millis));
+
+        // 并行操作
+        t0 = System.nanoTime();
+
+        count = values.parallelStream().sorted().count();
+        System.out.println(count);
+
+        t1 = System.nanoTime();
+
+        millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
+        System.out.println(String.format("parallel sort took: %d ms", millis));
+    }
+
     public static void main(String[] args) {
+        filter();
+        sort();
+        map();
+        match();
+        count();
+        reduce();
+        parallel();
     }
 }
