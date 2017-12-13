b752e63c30d90f224c2fe25621cded20c5344312
willWu <willwu618@gmail.com>
2017-12-13 22:25:34 +0800

    [ADD]Base64

---
+++ src/com/will/base64/BaseDemo.java
@@ -0,0 +1,25 @@
+package com.will.base64;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+/**
+ * ClassName:BaseDemo
+ * Description:Base64加密解密
+ *
+ * @Author Will Wu
+ * @Email willwu618@gmail.com
+ * @Date 2017-12-13
+ */
+public class BaseDemo {
+    public static void main(String[] args) {
+        final String text = "Base64 finally in Java 8!";
+
+        final String encoded = Base64.getEncoder()
+                .encodeToString(text.getBytes(StandardCharsets.UTF_8));
+        System.out.println(encoded);
+
+        final String decoded = new String(Base64.getDecoder().decode(encoded), StandardCharsets.UTF_8);
+        System.out.println(decoded);
+    }
+}
