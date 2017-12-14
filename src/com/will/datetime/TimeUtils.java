bd798c08b1d9f98b06b294184959aaa556faf73f
wuguanghui <willwu618@gmail.com>
2017-12-14 12:08:10 +0800

    [MOD]基于jdk8的时间工具类（2）

--- src/com/will/datetime/TimeUtils.java
+++ src/com/will/datetime/TimeUtils.java
@@ -2,6 +2,7 @@ package com.will.datetime;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  * ClassName:TimeUtils
@@ -18,6 +19,26 @@ public final class TimeUtils {
 
     private TimeUtils() {
         // no construct function
+    }
+
+    /**
+     * 获取指定时间的指定格式
+     * @param time
+     * @param pattern
+     * @return
+     */
+    public static String formatTime(LocalDateTime time,String pattern) {
+        return time.format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    /**
+     * 获取当前时间的指定格式
+     * @param time
+     * @param pattern
+     * @return
+     */
+    public static String formatNow(String pattern) {
+        return  formatTime(LocalDateTime.now(), pattern);
     }
 
     /**
@@ -40,7 +61,7 @@ public final class TimeUtils {
     }
 
     /**
-     * 时间转 String
+     * 时间按默认格式转String，例：2017-12-14 12:00:00
      * @param time
      * @return
      */
@@ -59,24 +80,122 @@ public final class TimeUtils {
     }
 
     /**
-     * 获取当前时间
+     * 获取默认格式的当前时间，例：2017-12-14 12:00:00
      * @return
      */
     public static String getCurrentDatetime() {
-        return DEFAULT_DATETIME_FORMATTER.format(LocalDateTime.now());
+        return parseTime(LocalDateTime.now());
     }
 
     /**
-     * 获取当前时间
+     * 获取指定格式的当前时间
      * @param format 时间格式
      * @return
      */
     public static String getCurrentDatetime(TimeFormat format) {
-        return format.formatter.format(LocalDateTime.now());
+        return parseTime(LocalDateTime.now(), format);
     }
 
-    // TODO 工具类 http://blog.csdn.net/KingBoyWorld/article/details/75808108
+    /**
+     * Date转换为LocalDateTime
+     * @param date
+     * @return
+     */
+    public static LocalDateTime convertDateToLocalDateTime(Date date) {
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
 
+    }
+
+    /**
+     * LocalDateTime转换为Date
+     * @param time
+     * @return
+     */
+    public static Date convertLocalDateTimeToDate(LocalDateTime time) {
+        return Date.from(time.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    /**
+     * 获取指定日期的毫秒
+     * @param time
+     * @return
+     */
+    public static Long getMilliByTime(LocalDateTime time) {
+        return time.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    }
+
+    /**
+     * 获取指定日期的秒
+     * @param time
+     * @return
+     */
+    public static Long getSecondsByTime(LocalDateTime time) {
+        return time.atZone(ZoneId.systemDefault()).toInstant().getEpochSecond();
+    }
+
+    /**
+     * 日期加上一个数,根据field不同加不同值,field为ChronoUnit.*
+     * @param time
+     * @param number
+     * @param field 单位(年月日时分秒)
+     * @return
+     */
+    public static LocalDateTime plus(LocalDateTime time, long number, TemporalUnit field) {
+        return time.plus(number, field);
+    }
+
+    /**
+     * 日期减去一个数,根据field不同减不同值,field参数为ChronoUnit.*
+     * @param time
+     * @param number
+     * @param field 单位(年月日时分秒)
+     * @return
+     */
+    public static LocalDateTime minu(LocalDateTime time, long number, TemporalUnit field){
+        return time.minus(number,field);
+    }
+
+    /**
+     * 获取两个日期的差  field参数为ChronoUnit.*
+     * @param startTime
+     * @param endTime
+     * @param field 单位(年月日时分秒)
+     * @return
+     */
+    public static long betweenTwoTime(LocalDateTime startTime, LocalDateTime endTime, ChronoUnit field) {
+        Period period = Period.between(LocalDate.from(startTime), LocalDate.from(endTime));
+        if (field == ChronoUnit.YEARS) {
+            return period.getYears();
+        }
+        if (field == ChronoUnit.MONTHS) {
+            return period.getYears() * 12 + period.getMonths();
+        }
+        return field.between(startTime, endTime);
+    }
+
+    /**
+     * 获取一天的开始时间，例：2017,12,14 00:00
+     * @param time
+     * @return
+     */
+    public static LocalDateTime getDayStart(LocalDateTime time) {
+        return time.withHour(0)
+                .withMinute(0)
+                .withSecond(0)
+                .withNano(0);
+    }
+
+    /**
+     * 获取一天的结束时间，例：2017,12,14 23:59:59.999999999
+     * @param time
+     * @return
+     */
+    public static LocalDateTime getDayEnd(LocalDateTime time) {
+        return time.withHour(23)
+                .withMinute(59)
+                .withSecond(59)
+                .withNano(999999999);
+    }
 
     /**
      * 时间格式
